fc42523a1d4c18210297eefb9a29baf70116f003
dalulu91 <danielluras@live.no>
2024-10-09 09:12:45 +0200

    Made a Class for each frame column

--- app.py
+++ app.py
@@ -20,102 +20,101 @@ class MainWindow(ctk.CTk):
         self.title("Color Picker")
         self.resizable(False, False)
 
-        # Left Column Frame
-        self.leftFrame = ctk.CTkFrame(self)
-        self.leftFrame.grid(row=0, column=0, sticky="n", pady=5, padx=5)
-
-        ctk.CTkLabel(self.leftFrame, text="Saved Palettes").grid(row=0, pady=5, padx=5)
-
-        self.presetBox = tk.Listbox(self.leftFrame, bg="#222222")
-        self.presetBox.grid(row=1, pady=5, padx=5)
-
-        # Middle Column Frame
-        self.midFrame = ctk.CTkFrame(self)
-        self.midFrame.grid(row=0, column=1, sticky="n", pady=5, padx=5)
-
-        # Button
-        self.pickColorBtn = ctk.CTkButton(
-            self.midFrame, text="Pick Color", command=self.pick_color
-        )
-        self.pickColorBtn.grid(row=0, columnspan=2, pady=10, padx=5)
-
-        # Textfields HEX and RGB
-        ctk.CTkLabel(self.midFrame, text="HEX:").grid(row=1, columnspan=2, padx=5)
-        self.hexEntry = ctk.CTkEntry(self.midFrame)
-        self.hexEntry.grid(row=2, columnspan=2, pady=5, padx=5)
-
-        ctk.CTkLabel(self.midFrame, text="RGB:").grid(row=3, columnspan=2, padx=5)
-        self.rgbEntry = ctk.CTkEntry(self.midFrame)
-        self.rgbEntry.grid(row=4, columnspan=2, pady=5, padx=5)
-
-        self.checkClipBoard = ctk.CTkCheckBox(
-            self.midFrame, text="Auto copy to clipboard"
-        )
-        self.checkClipBoard.grid(row=5, columnspan=2, pady=5, padx=5)
-        self.checkClipBoard.select()  # Make a config so the user can turn this off as default
-
-        self.selectedRadio = tk.StringVar()
-        self.hexRadio = ctk.CTkRadioButton(
-            self.midFrame, text="HEX", value="hex", variable=self.selectedRadio
-        )
-        self.hexRadio.grid(row=6, column=0, pady=5, padx=5)
-        self.hexRadio.select()  # Make a config so the user can change this as default
-
-        self.rgbRadio = ctk.CTkRadioButton(
-            self.midFrame, text="RGB", value="rgb", variable=self.selectedRadio
-        )
-        self.rgbRadio.grid(row=6, column=1, pady=5, padx=5)
-
-        # Right Column Frame
-        self.rightFrame = ctk.CTkFrame(self)
-        self.rightFrame.grid(row=0, column=2, pady=5, padx=5)
-
-        ctk.CTkLabel(self.rightFrame, text="Current Palette").grid(
-            row=0, pady=5, padx=5
-        )
-        self.paletteBox = tk.Listbox(self.rightFrame, bg="#222222", fg="#dedede")
-        self.paletteBox.grid(row=1, pady=5, padx=5)
-        self.paletteBox.insert(tk.END, " ")
-
-        # Save palette Entry and Button
-        self.paletteEntryName = ctk.CTkEntry(
-            self.rightFrame, placeholder_text="Palette name ..."
-        )
-        self.paletteEntryName.grid(row=2, pady=5, padx=5)
-
-        self.savePaletteBtn = ctk.CTkButton(self.rightFrame, text="Save")
-        self.savePaletteBtn.grid(row=3, pady=5, padx=5)
-
-        self.clearPaletteBtn = ctk.CTkButton(
-            self.rightFrame, text="Clear", fg_color="#e02c2c", hover_color="#ff4848"
-        )
-        self.clearPaletteBtn.grid(row=4, pady=5, padx=5)
+        # Create frames
+        self.leftFrame = LeftFrameColumn(self)
+        self.midFrame = MidFrameColumn(self, self.pick_color)
+        self.rightFrame = RightFrameColumn(self)
 
     def pick_color(self):
         # reset textfields
-        self.hexEntry.delete(0, tk.END)
-        self.rgbEntry.delete(0, tk.END)
+        self.midFrame.hexEntry.delete(0, tk.END)
+        self.midFrame.rgbEntry.delete(0, tk.END)
 
         # open color dialog
         colors = askcolor(title="Pick Color")
 
         # update textfields with the new color
-        self.hexEntry.insert(0, colors[1])
-        self.rgbEntry.insert(0, colors[0])
+        self.midFrame.hexEntry.insert(0, colors[1])
+        self.midFrame.rgbEntry.insert(0, colors[0])
 
         # add color to palette
-        self.paletteBox.insert(tk.END, colors[1])
-        self.paletteBox.itemconfig(tk.END, {"bg": colors[1]})
+        self.rightFrame.paletteBox.insert(tk.END, colors[1])
+        self.rightFrame.paletteBox.itemconfig(tk.END, {"bg": colors[1]})
 
 
 class LeftFrameColumn(ctk.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
 
+        # Left Column Frame
+        self.grid(row=0, column=0, sticky="n", pady=5, padx=5)
+        ctk.CTkLabel(self, text="Saved Palettes").grid(row=0, pady=5, padx=5)
+        self.presetBox = tk.Listbox(self, bg="#222222")
+        self.presetBox.grid(row=1, pady=5, padx=5)
+
+
+class MidFrameColumn(ctk.CTkFrame):
+    def __init__(self, master, pick_color_callback):
+        super().__init__(master)
+
+        # Middle Column Frame
+        self.grid(row=0, column=1, sticky="n", pady=5, padx=5)
+
+        # Button
+        self.pickColorBtn = ctk.CTkButton(
+            self, text="Pick Color", command=pick_color_callback
+        )
+        self.pickColorBtn.grid(row=0, columnspan=2, pady=10, padx=5)
+
+        # Textfields HEX and RGB
+        ctk.CTkLabel(self, text="HEX:").grid(row=1, columnspan=2, padx=5)
+        self.hexEntry = ctk.CTkEntry(self)
+        self.hexEntry.grid(row=2, columnspan=2, pady=5, padx=5)
+
+        ctk.CTkLabel(self, text="RGB:").grid(row=3, columnspan=2, padx=5)
+        self.rgbEntry = ctk.CTkEntry(self)
+        self.rgbEntry.grid(row=4, columnspan=2, pady=5, padx=5)
+
+        self.checkClipBoard = ctk.CTkCheckBox(self, text="Auto copy to clipboard")
+        self.checkClipBoard.grid(row=5, columnspan=2, pady=5, padx=5)
+        self.checkClipBoard.select()  # Make a config so the user can turn this off as default
+
+        self.selectedRadio = tk.StringVar()
+        self.hexRadio = ctk.CTkRadioButton(
+            self, text="HEX", value="hex", variable=self.selectedRadio
+        )
+        self.hexRadio.grid(row=6, column=0, pady=5, padx=5)
+        self.hexRadio.select()  # Make a config so the user can change this as default
+
+        self.rgbRadio = ctk.CTkRadioButton(
+            self, text="RGB", value="rgb", variable=self.selectedRadio
+        )
+        self.rgbRadio.grid(row=6, column=1, pady=5, padx=5)
+
 
 class RightFrameColumn(ctk.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
+
+        # Right Column Frame
+        self.grid(row=0, column=2, pady=5, padx=5)
+
+        ctk.CTkLabel(self, text="Current Palette").grid(row=0, pady=5, padx=5)
+        self.paletteBox = tk.Listbox(self, bg="#222222", fg="#dedede")
+        self.paletteBox.grid(row=1, pady=5, padx=5)
+        self.paletteBox.insert(tk.END, " ")
+
+        # Save palette Entry and Button
+        self.paletteEntryName = ctk.CTkEntry(self, placeholder_text="Palette name ...")
+        self.paletteEntryName.grid(row=2, pady=5, padx=5)
+
+        self.savePaletteBtn = ctk.CTkButton(self, text="Save")
+        self.savePaletteBtn.grid(row=3, pady=5, padx=5)
+
+        self.clearPaletteBtn = ctk.CTkButton(
+            self, text="Clear", fg_color="#e02c2c", hover_color="#ff4848"
+        )
+        self.clearPaletteBtn.grid(row=4, pady=5, padx=5)
 
 
 if __name__ == "__main__":
